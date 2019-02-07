1ee8744d49bf1fe317af70320bc763175c216834
peter sigurdson <peter@petersigurdson.net>
2019-02-07 13:23:14 -0500

    Add project files.

---
+++ C0888888/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C0888888
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
