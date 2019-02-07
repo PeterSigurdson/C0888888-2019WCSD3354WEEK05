862f145c515630a58c4daa0135bd2fb1caae2aa2
peter sigurdson <peter@petersigurdson.net>
2019-02-07 14:05:18 -0500

    2:05 PM COMMIT

--- C0888888/Program.cs
+++ C0888888/Program.cs
@@ -6,7 +6,32 @@ namespace C0888888
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
+        }
+    }
+
+    class TestQuestion2
+    {
+        public int myFavoriteVariable = 0; 
+
+        public void PlayingWithForLoops()
+        {
+            // write a For Loop to Add 10 Numbers
+            for ( ; MyMethod() ;  )
+            {
+                if (myFavoriteVariable > 10)
+                {
+                    Console.WriteLine("i am so out of here!");
+                    break;
+                }
+                Console.WriteLine("oh no I have to go through this stupid loop again...");
+            }
+        }
+        public bool MyMethod()
+        {
+            myFavoriteVariable++;
+            return true;
         }
     }
 }
